dc2129eef43e90b47e0610df8ca540cd987269ff
salm06 <maucerisalvatore06@gmail.com>
2017-12-02 12:31:50 +0100

    Merging

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Home</title>
+    <title>Quarta Dimensione - Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <?=include('templates/mainStyle.php')?>
   </head>
   <body>

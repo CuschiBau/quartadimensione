075c5adb3543718657d1b474adf5ea7fde787ad3
CuschiBau <mattiacibau@libero.it>
2017-12-10 00:33:19 +0100

    sliding bar shit

--- barlist.php
+++ barlist.php
@@ -6,55 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <?=include('templates/mainStyle.php')?>
-    <link rel="stylesheet" href="static/css/bar.css?a=1">
+    <link rel="stylesheet" type="text/css" href="static/css/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="static/css/slick-theme.css"/>
+    <link rel="stylesheet" href="static/css/bar.css?a=4">
     <?php
       ob_start();
     ?>
   </head>
   <body>
     <?php include('templates/header.html');?>
-    <h1>Food & Drinks</h1>
+    <h1 class="main_title container">Food & Drinks</h1>
 
-    <div class="container text_center">
+    <div class="container text_center menu_container">
         <div class="row">
-            <div class="menu_selector col selected_menu" data-selector="#panini">PANINI</div>
-            <div class="menu_selector col" data-selector="#bevande">BEVANDE</div>
-            <div class="menu_selector col" data-selector="#snacks">SNACKS</div>
-            <div class="menu_selector col" data-selector="#dolci">DOLCI</div>
+            <div class="menu_selector col selected_menu" data-selector="0">PANINI</div>
+            <div class="menu_selector col" data-selector="1">BEVANDE</div>
+            <div class="menu_selector col" data-selector="2">SNACKS</div>
+            <div class="menu_selector col" data-selector="3">DOLCI</div>
         </div>
     </div>
 
-    <section class="menu_lists">
-        
-        <?php
-            $menu = simplexml_load_file('templates/bar.xml');
-            foreach($menu->sezione as $section){
-                $display = $section["data-name"] != 'panini' ? 'hide' : '';
-                echo '<div class="container item-cont '. $display .'" id="'.$section["data-name"].'">';
-                    foreach($section->elemento as $elemento){
-                    ?>
+    <div class="menu_lists container">        
+    <?php
+        $menu = simplexml_load_file('templates/bar.xml');
+        foreach($menu->sezione as $section){
+            $display = $section["data-name"] != 'panini' ? '' : '';
+            echo '<div class="item_cont '. $display .'" id="'.$section["data-name"].'">';
+                foreach($section->elemento as $elemento){
+                ?>
 
-                    <div>
-                        <div class="row">
-                            <div class="col">
-                                <div><?=$elemento->nome?></div>
-                            </div>
-                            <div class="col text_right">
-                            <?=$elemento->prezzo?>
-                            </div>
+                <div>
+                    <div class="row bar_nameprice">
+                        <div class="col">
+                            <div><?=$elemento->nome?></div>
                         </div>
-                        <div><?=$elemento->ingredienti?></div>
+                        <div class="col text_right">
+                        <?=$elemento->prezzo?>
+                        </div>
                     </div>
-                        
-                    <?php
-                    }
-                echo '</div>';
-            }
-        ?>
-       
-    </section>
+                    <div class="bar_ingredients"><?=$elemento->ingredienti?></div>
+                </div>
+                    
+                <?php
+                }
+            echo '</div>';
+        }
+    ?>
+    </div>
 
     <?=include('templates/footer.php');?>
-    <script type="text/javascript" src="static/js/barlist.js"></script>
+    <script type="text/javascript" src="static/js/slick.min.js"></script>
+    <script type="text/javascript" src="static/js/barlist.js?a=3"></script>
   </body>
 </html>
--- static/js/barlist.js
+++ static/js/barlist.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
 
-    var mH = 0
-    $('.menu_lists .item-cont').each(function(){
-        console.log($(this).height())
-        if($(this).height() > mH) mH = $(this).height()
-    })
-    $('.menu_lists').height(mH+'px')
+    $(".menu_lists").slick({
+        arrows: false
+    });
 
     $(document).on('click','.menu_selector',function(){
-        $('.item-cont').each(function(){$(this).addClass('hide')})
-        $($(this).data('selector')).removeClass('hide')
+        $(".menu_lists").slick('slickGoTo', $(this).data('selector'))       
         $('.menu_selector').each(function(){$(this).removeClass('selected_menu')})
-        $(this).addClass('selected_menu')
+        $(this).addClass('selected_menu') 
     })
+
+    $('.menu_lists').on('swipe', function(event, slick, direction){
+        var c = $(this).slick('slickCurrentSlide')
+        $('.menu_selector').each(function(){$(this).removeClass('selected_menu')})
+        $('.menu_selector[data-selector="'+c+'"]').addClass('selected_menu') 
+      });
 
 })

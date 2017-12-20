4abefba0ec3e50f3be63ab6e08e2c1f49d204610
Salvatore Mauceri <salvatore.mauceri@usablenet.com>
2017-12-20 18:21:42 +0100

    carousel fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <?=include('templates/mainStyle.php')?>
     <link rel="stylesheet" type="text/css" href="static/css/slick.css"/>
     <link rel="stylesheet" type="text/css" href="static/css/slick-theme.css"/>
-    <link rel="stylesheet" type="text/css" href="static/css/index.css?update=4"/>
+    <link rel="stylesheet" type="text/css" href="static/css/index.css?update=5"/>
 
   </head>
   <body>
